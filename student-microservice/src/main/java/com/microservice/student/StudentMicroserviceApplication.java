a9fa6799ad678758cebdb2a1f366b409ac870d3b
Estefann Loaiza <stefannloa@gmail.com>
2023-12-12 15:48:08 -0500

    CHANGED Clean

--- student-microservice/src/main/java/com/microservice/student/StudentMicroserviceApplication.java
+++ student-microservice/src/main/java/com/microservice/student/StudentMicroserviceApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class StudentMicroserviceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(StudentMicroserviceApplication.class, args);
 	}
-
 }
